fee2d2bcbb749e8fe11ff90781f143ef77dae777
T-Tavares <thiago.tavares.freire@gmail.com>
2023-12-08 11:53:27 +1300

    Adding new Branch to Git

--- 2ndTest.jsx
+++ 2ndTest.jsx
@@ -1,1 +1,15 @@
-test number 2
+// Shifting code test
+
+test number 2 -> editing existing code test.
+
+/* 
+
+lorem  
+
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti vero, accusamus amet natus illo eos, laborum vitae odit, illum fugiat doloribus nulla saepe ab? Deleniti distinctio facere quas laborum reprehenderit!
+Commodi maiores natus veniam sed praesentium deleniti dolores, velit ab recusandae fuga debitis blanditiis ratione, placeat aliquam. Reiciendis at deleniti perspiciatis perferendis temporibus dolorum, tempora quam repudiandae et quae amet.
+Ratione aliquid minima adipisci nisi sit iusto, molestias sed ducimus! Quos iusto consectetur veniam reiciendis doloribus officiis eos cupiditate, expedita officia voluptatibus dolores quaerat explicabo nulla sapiente, similique autem a.
+Natus aliquam eligendi corrupti necessitatibus possimus. Nam dolorem quos obcaecati accusantium laborum deleniti tenetur quaerat porro quod modi, deserunt libero culpa fugiat, voluptatem dolorum, exercitationem numquam in laudantium sed ullam.
+Veritatis iste facere, suscipit amet voluptatem cumque? Cupiditate dolorem perferendis quo possimus mollitia rem atque, aut, velit aliquam iusto eum quis, deleniti placeat necessitatibus adipisci voluptates ut cum at obcaecati?
+
+*/
--- Test.jsx
+++ Test.jsx
@@ -1,1 +1,8 @@
+// Just moving stuff around 
+//
 this is a test for GIT
+
+/* 
+    Thiago Tavares 
+    Test for git branching 
+*/
